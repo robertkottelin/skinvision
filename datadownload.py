b0b3cd49e70a46860e1e4f208d27ca89728abac4
Robert Kottelin <robert.kottelin@gmail.com>
2023-07-01 20:10:36 +0200

    Update datadownload.py

--- datadownload.py
+++ datadownload.py
@@ -1,5 +1,5 @@
-isic image download images/  # downloads the entire archive, images and metadata, to images/
+# isic image download images/  # downloads the entire archive, images and metadata, to images/
 
-# optionally filter the results
-isic image download --search 'diagnosis:"basal cell carcinoma"' images/
-isic image download --search 'age_approx:[5 TO 25] AND sex:male' images/
+# # optionally filter the results
+# isic image download --search 'diagnosis:"basal cell carcinoma"' images/
+# isic image download --search 'age_approx:[5 TO 25] AND sex:male' images/
