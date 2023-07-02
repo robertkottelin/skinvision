de88f9a7c74589d381e47e0619e726afbab6f50b
Robert Kottelin <robert.kottelin@gmail.com>
2023-07-02 12:58:14 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 
 # Set up directories
 base_dir = 'data'
@@ -17,29 +17,39 @@ validation_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     os.path.join(base_dir, 'train'),  # directory should contain subdirectories 'benign' and 'malignant'
-    target_size=(150, 150),
+    target_size=(299, 299),
     batch_size=5,
     class_mode='binary')
 
 validation_generator = validation_datagen.flow_from_directory(
     os.path.join(base_dir, 'validation'),  # directory should contain subdirectories 'benign' and 'malignant'
-    target_size=(150, 150),
+    target_size=(299, 299),
     batch_size=5,
     class_mode='binary')
 
 
 # Set up model architecture
 model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(150, 150 ,3)),
-    MaxPooling2D(),
+    Conv2D(32, 3, padding='same', activation='relu', input_shape=(299, 299 ,3)),
     Conv2D(32, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Conv2D(64, 3, padding='same', activation='relu'),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(128, 3, padding='same', activation='relu'),
+    Conv2D(128, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(256, 3, padding='same', activation='relu'),
+    Conv2D(256, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Flatten(),
+    Dense(1024, activation='relu'),
+    Dropout(0.5),
     Dense(512, activation='relu'),
+    Dropout(0.5),
     Dense(1, activation='sigmoid')
 ])
+
 
 # Compile the model
 model.compile(optimizer='adam',
@@ -50,7 +60,7 @@ model.compile(optimizer='adam',
 history = model.fit(
     train_generator,
     steps_per_epoch=len(train_generator),
-    epochs=10,
+    epochs=5,
     validation_data=validation_generator,
     validation_steps=len(validation_generator)
 )
