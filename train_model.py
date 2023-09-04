ddb6b7787cf94232b94ef96d8f689d0b077e3c18
Robert Kottelin <robert.kottelin@gmail.com>
2023-09-04 17:54:13 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -62,7 +62,7 @@ model.compile(optimizer='adam',
 history = model.fit(
     train_generator,
     steps_per_epoch=len(train_generator),
-    epochs=5,
+    epochs=4,
     validation_data=validation_generator,
     validation_steps=len(validation_generator)
 )
